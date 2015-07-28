632a80db9266b326f17de8153319513449ce4d7f
Justin Krasner-Karpen <justinkk@Justins-MacBook-Air.local>
2015-07-28 11:57:27 -0700

    deleted debug code

--- Assets/Scripts/CheckBreathing.cs
+++ Assets/Scripts/CheckBreathing.cs
@@ -5,10 +5,7 @@ using System; //For Math.Max
 public class CheckBreathing : MonoBehaviour {
 	private Animator anim;
 	private AudioClip clip;
-	//private float currTime;
-	//private int physicsStepCount;
 
-	//public int stepsPerSample = 24; //# of physics steps before we check the microphone again
 	public int sampleLength = 1; //length of sample in seconds
 	public int sampleFreq = 44100;
 	public float whiteNoiseRatio = 0.6f;
@@ -24,13 +21,6 @@ public class CheckBreathing : MonoBehaviour {
 
 	// Update is called once per frame
 	void Update () {
-		/*
-		if (Input.anyKey) {
-			anim.SetBool("isBreathing", true);
-		} else {
-			anim.SetBool("isBreathing", false);
-		}
-		*/
 		//Calculate how many samples to  get
 		float timestep = Time.deltaTime;
 		int numSamples = (int) (timestep * sampleFreq);
@@ -46,12 +36,6 @@ public class CheckBreathing : MonoBehaviour {
 		float[] recentSamples = new float[recentLength];
 		Array.Copy(samples, startPosition, recentSamples, 0, recentLength);
 
-		//Test copying
-		/*string[] stringSamples = new string[recentLength];
-		for (int i = 0; i < recentLength; i++)
-			stringSamples[i] = recentSamples[i].ToString();
-		Debug.Log(string.Join(" ", stringSamples));*/
-
 		//Test if the sound since the last frame sounds like white noise (breathing out)
 		//Algorithm from http://stackoverflow.com/questions/3881256/can-you-programmatically-detect-white-noise
 		//White noise has higher ratio of absolute integral of derivative
@@ -64,9 +48,6 @@ public class CheckBreathing : MonoBehaviour {
 			derivIntegral += Math.Abs(recentSamples[i] - recentSamples[i-1]);
 		}
 
-		Debug.Log("ratio: " + derivIntegral / integral);
-		Debug.Log("integral: " + integral);
-
 		//You are breathing out
 			//Noise-ish enough                          Loud enough            
 		if (derivIntegral / integral > whiteNoiseRatio && integral > integralThreshold) {
@@ -75,29 +56,5 @@ public class CheckBreathing : MonoBehaviour {
 		else {
 			anim.SetBool("isBreathing", false);
 		}
-		//wNoiseRatio = ?0.8; //quite easily discovered, bit tricky to calculate.
-
-		/*if((sumd1/sumd0)<wNoiseRatio)
-		{ / }*/
 	}
-
-	// FixedUpdate is called once per physics step
-	/*void FixedUpdate () {
-		physicsStepCount++;
-		//Every stepsPerSample steps, chekc the microphone for breathing
-		if (physicsStepCount > stepsPerSample) {
-			float[] samples = new float[clip.samples * clip.channels];
-			clip.GetData(samples, 0);
-
-			int position = Microphone.getPosition();
-
-			//string[] stringSamples = new string[samples.Length];
-			for (int i = 0; i < samples.Length; i++)
-				stringSamples[i] = samples[i].ToString();
-			Debug.Log(string.Join(" ", stringSamples));
-		}
-	}*/
-
-	//Algorithm from http://stackoverflow.com/questions/3881256/can-you-programmatically-detect-white-noise
-	//Assumes breath out sounds like white noise
 }
